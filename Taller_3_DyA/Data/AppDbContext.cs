94dca0c969fcb0427a08620f27c9bd18552adc27
Angel <angeldavidstudios@outlook.com>
2024-11-01 04:52:38 -0500

    Migraciones exitosas

--- Taller_3_DyA/Data/AppDbContext.cs
+++ Taller_3_DyA/Data/AppDbContext.cs
@@ -3,13 +3,32 @@ using Server.Models;
 
 namespace Taller_3_DyA.Data;
 
-public class AppDbContext: DbContext
+public class AppDbContext : DbContext
 {
     public DbSet<ProductV1> ProductV1 { get; set; }
     public DbSet<ProductV2> ProductV2 { get; set; }
     public DbSet<ProductV3> ProductV3 { get; set; }
-    
+
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ProductV1>()
+            .Property(p => p.Price)
+            .HasColumnType("decimal(18,2)"); // Specify the SQL server column type with precision and scale
+        
+        modelBuilder.Entity<ProductV2>()
+            .Property(p => p.Price)
+            .HasColumnType("decimal(18,2)"); // Specify the SQL server column type with precision and scale
+
+        modelBuilder.Entity<ProductV3>()
+            .Property(p => p.Discount)
+            .HasColumnType("decimal(18,2)"); // Specify the SQL server column type with precision and scale
+
+        modelBuilder.Entity<ProductV3>()
+            .Property(p => p.FinalPrice)
+            .HasColumnType("decimal(18,2)"); // Specify the SQL server column type with precision and scale
     }
 }
