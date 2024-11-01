8b29debda5bc349389a13af701a217ee8bd59378
Angel <angeldavidstudios@outlook.com>
2024-11-01 04:33:50 -0500

    Preparacion de DbContext para migraciones

---
+++ Taller_3_DyA/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Taller_3_DyA.Data;
+
+public class AppDbContext: DbContext
+{
+    public DbSet<ProductV1> ProductV1 { get; set; }
+    public DbSet<ProductV2> ProductV2 { get; set; }
+    public DbSet<ProductV3> ProductV3 { get; set; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+}
